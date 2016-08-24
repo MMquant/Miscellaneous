735b57425fd6b2ee72c0289a47494c74d878c3b3
Petr Javorik <MMquant@users.noreply.github.com>
2016-08-24 21:32:45 +0200

    v1.0

---
+++ histvol.m
@@ -0,0 +1,35 @@
+function [ sigma_hat ] = histvol( y,k )
+%histvol() Computes historical volatility
+%   histvol() function computes historical volatility given input vector y
+%   and window size k.
+
+% Petr Javorik (2016) maple@mmquant.net
+
+% input check
+assert(k <= length(y),'Window length is greater than time series length!');
+
+% returns
+ylag1 = [0;y(1:end-1)];
+y(1) = [];
+ylag1(1) = [];
+r = log(y./ylag1);
+
+% sample mean mu_hat(t)
+mu_hat = zeros(size(r));
+for t = k+1:length(r)
+    
+    mu_hat(t,1) = sum(r(t-k:t-1)) / k;
+    
+end
+
+% historical volatility (sample variance)
+sigma_hat = zeros(size(r));
+for t = k+1:length(r)
+    
+    sigma_hat(t,1) = sum((r(t-k:t-1) - mu_hat(t)).^2) / (k-1);
+    
+end
+
+
+end
+
