c8c330edb4db0b381a269cc892edcf1d30e1ebf8
Petr Javorik <MMquant@users.noreply.github.com>
2016-09-06 16:44:22 +0200

    v1.0

---
+++ PEvol.m
@@ -0,0 +1,32 @@
+function sigma_hat = PEvol(H,L,k)
+%PEvol() Computes historical volatility using Parkinson estimator
+%   PEvol() function computes historical volatility using Parkinson
+%   estimator and extreme price values.
+
+% INPUT:
+%   H   - highs
+%   L   - lows
+%   k   - rolling window size. integer
+
+% OUTPUT:
+%   out.sigma_hat       - historical volatility estimate
+
+
+%   Petr Javorik (2016) maple@mmquant.net   http://mmquant.net/introduction-to-volatility-models/
+
+
+% input check
+assert(k <= length(H),'Window length is greater than time series length!');
+assert(length(H)==length(L),'Sizes of High and Low series are not equal');
+
+% PE computation
+sigma_hat = zeros(size(H));
+sqlogdiff = log(H./L).^2;
+for t = k+1:length(H)
+    
+    sigma_hat(t,1) = sqrt(1/4*k*log(2) * sum(sqlogdiff(t-k:t-1)));
+    
+end
+
+
+end
